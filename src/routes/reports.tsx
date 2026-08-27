3207ae043d7f6ce2e7bd51ccc23b4bd5143d5978
AjmalKhan <126456220+akhive@users.noreply.github.com>
2026-08-27 11:14:06 +0400

    Update reports.tsx

--- src/routes/reports.tsx
+++ src/routes/reports.tsx
@@ -882,9 +882,7 @@ function ReportsPage() {
             <CardHeader className="flex flex-row items-start justify-between gap-4">
               <div>
                 <CardTitle className="text-base">Expired contracts</CardTitle>
-                <CardDescription>
-                  </span>
-                </CardDescription>
+                <CardDescription>{expired.length} contract(s)</CardDescription>
               </div>
               <ReportActions
                 onExport={() =>
